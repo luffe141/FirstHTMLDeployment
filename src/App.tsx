cde59eb03eb633947e356f3600609c23a6399685
luffe141 <luffeerder@gmail.com>
2026-02-13 10:01:28 +0100

    hej

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,10 @@
+import { Container } from "@mui/material";
 import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
-  Container,
-  CssBaseline,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
 
 export default function App() {
   return (
     <>
-      <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Mit MUI App
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Container sx={{ mt: 4 }}>
-        <Typography variant="h4" gutterBottom>
-          Velkommen til React + MUI
-        </Typography>
-        <Button variant="contained" color="primary">
-          Tryk her
-        </Button>
-      </Container>
+      <Container></Container>
     </>
   );
 }
